e6f6898787a0da867945d2fcff6037729ba49fad
kwongio <rldh11111@naver.com>
2024-11-09 15:01:07 +0900

    feat: promotion 추가

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,58 @@
+package store.domain;
+
+import java.time.LocalDate;
+
+public class Promotion {
+    private String name;
+    private int buy;
+    private int get;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Promotion{" +
+                "name='" + name + '\'' +
+                ", buy=" + buy +
+                ", get=" + get +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+
+    public boolean isRangePromotion( LocalDate now) {
+        return now.isAfter(startDate) && now.isBefore(endDate);
+    }
+
+    public int sell(int quantity) {
+        return quantity / (buy + get); // 6개일때 2+1이면 2개
+    }
+}
