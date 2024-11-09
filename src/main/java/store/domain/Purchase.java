1c8fba3cb206c3fafa55cdff296d420ca13b1f93
kwongio <rldh11111@naver.com>
2024-11-09 15:02:37 +0900

    feat: purchase 추가

---
+++ src/main/java/store/domain/Purchase.java
@@ -0,0 +1,41 @@
+package store.domain;
+
+public class Purchase {
+    private final String name;
+    private final int quantity;
+    private final int price;
+    private int promotionQuantity;
+
+    public Purchase(String name, int quantity, int price, int promotionQuantity) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.promotionQuantity = promotionQuantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getPromotionQuantity() {
+        return promotionQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", promotionQuantity=" + promotionQuantity +
+                '}';
+    }
+}
